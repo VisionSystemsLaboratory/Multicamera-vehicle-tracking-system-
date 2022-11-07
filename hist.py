472654110851c63e2822254267c60f380da20690
Rafal711 <61950260+Rafal711@users.noreply.github.com>
2022-11-07 22:53:06 +0100

    Update hist.py

--- hist.py
+++ hist.py
@@ -3,11 +3,12 @@ import cv2
 import matplotlib.pyplot as plt
 
 def histogramRGB(image, mask):
-    objImg = image * mask
+    maskRGB = np.stack((mask, mask, mask), axis=2)
+    objImg = image * maskRGB
     histogramRGB = np.zeros((256, 3))
     binEdgesRGB = np.zeros((257, 3))
     
-    background = np.count_nonzero(mask[:, :, 0]==0)
+    background = np.count_nonzero(mask==0)
     colors = ("red", "green", "blue")
 
     for channel_id, color in enumerate(colors):
@@ -18,21 +19,42 @@ def histogramRGB(image, mask):
         histogramRGB[:, channel_id] = histogram
         binEdgesRGB[:, channel_id] = binEdges
 
-    fig, ax = plt.subplots()
-    ax.set_xlim([0, 256])
-    for i in range(3):
-        plt.plot(binEdgesRGB[0:-1, i], histogramRGB[:, i], colors[i])
-    ax.set_title("Color Histogram")
-    ax.set_xlabel("Color value")
-    ax.set_ylabel("Pixel count")
+    # fig, ax = plt.subplots()
+    # ax.set_xlim([0, 256])
+    # for i in range(3):
+    #     plt.plot(binEdgesRGB[0:-1, i], histogramRGB[:, i], colors[i])
+    # ax.set_title("Color Histogram")
+    # ax.set_xlabel("Color value")
+    # ax.set_ylabel("Pixel count")
+    return histogramRGB
 
 
+# frame RGB, mask - gray scale (0, 255)
+def getHistogramsRGB(frame, mask):
+    retval, MaskCv = cv2.connectedComponents(mask)
+    # plt.figure(figsize=(13, 6))
+    # plt.imshow(MaskCv, cmap='plasma')
+    # plt.axis('off')
+    # plt.show()
+    histsRGB = []
+    for idx in range(1, retval):
+        maskIdx = np.where(MaskCv == idx, 1, 0)
+        histsRGB.append(histogramRGB(frame, maskIdx))
+    return histsRGB
+        
+    
+        
 
-img = cv2.imread("resorak.jpg")
+img = cv2.imread("shapes.png")
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-tempMask = np.zeros_like(img)
-tempMask[250:550, :] = 1
 
-# plt.figure()
-# plt.imshow(img)
-histogramRGB(img, tempMask)
+imgMask = cv2.imread("shapesMask.png")
+imgMask = cv2.cvtColor(imgMask, cv2.COLOR_BGR2RGB)
+imgMask = imgMask[:, :, 0]
+
+fig, ax = plt.subplots(1, 2)
+ax[0].imshow(img)
+ax[1].imshow(imgMask, cmap='gray')
+
+getHistogramsRGB(img, imgMask)
+
